179352ed66d9861e0ae0732d9ae2f291b17eb3f5
kish3023 <judykirungu@gmail.com>
2025-09-17 18:14:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './'   // ✅ Important for Netlify (prevents blank page issue)
 })
